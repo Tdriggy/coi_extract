1cfb9263b44a9bf6b787953a889f11fe40aef8aa
Tyler Driggers <wtdrigg@gmail.com>
2022-08-29 00:44:35 -0500

    some small updates

--- accord_extractor.py
+++ accord_extractor.py
@@ -26,33 +26,33 @@ class AccordExtractor:
                        'insurer_e':        (4.91, 7.90, 6.69, 7.90),
                        'insurer_f':        (4.91, 7.73, 6.69, 7.73),
                        'gl_pol_number':    (2.95, 5.75, 4.58, 6.73),
-                       'gl_eff_date':      (4.89, 5.84, 4.89, 6.79),
+                       'gl_eff_date':      (4.65, 5.82, 5.16, 6.82),
                        'gl_exp_date':      (5.25, 5.84, 5.80, 6.80),
                        'gl_each_occur':    (7.37, 6.73, 8.17, 6.76),
                        'gl_op_agg':        (7.31, 5.87, 7.80, 5.87),
-                       'gl_insurer':       (0.27, 5.91, 0.45, 6.69),
+                       'gl_insurer':       (0.27, 5.91, 0.45, 6.81),
                        'al_pol_number':    (2.95, 4.90, 4.58, 5.57),
                        'al_eff_date':      (4.86, 5.02, 4.86, 5.56),
                        'al_exp_date':      (5.25, 4.90, 5.90, 5.57),
                        'al_comb_limit':    (7.29, 5.40, 8.25, 5.57),
-                       'al_insurer':       (0.27, 5.07, 0.43, 5.53),
+                       'al_insurer':       (0.27, 5.07, 0.43, 5.65),
                        'umb_pol_number':   (2.95, 4.40, 4.61, 4.73),
                        'umb_eff_date':     (4.61, 4.40, 5.25, 4.73),
                        'umb_exp_date':     (5.25, 4.40, 5.90, 4.73),
                        'umb_each_occur':   (7.51, 4.71, 8.15, 4.77),
-                       'umb_insurer':      (0.27, 4.41, 0.44, 4.70),
+                       'umb_insurer':      (0.27, 4.41, 0.44, 4.81),
                        'wc_pol_number:':   (3.00, 3.64, 4.52, 4.24),
                        'wc_eff_date':      (4.60, 3.80, 5.25, 4.24),
                        'wc_exp_date':      (5.26, 3.80, 5.90, 4.24),
                        'wc_each_acci':     (7.31, 4.00, 8.25, 4.07),
                        'wc_each_emp':      (7.31, 3.85, 8.24, 3.90),
                        'wc_pol_limit':     (7.43, 3.74, 8.12, 3.76),
-                       'wc_insurer':       (0.28, 3.84, 0.43, 4.19),
+                       'wc_insurer':       (0.28, 3.84, 0.43, 4.32),
                        }
 
     def chromedriver_setup(self):
         my_options = webdriver.ChromeOptions()
-        my_options.add_argument('user-data-dir=C:/Users/driggerst/python/coi_extract/User Data')
+        my_options.add_argument('user-data-dir=C:/Users/Tyler/ProgramingProjects/coi_extract/User Data')
         driver_setup = webdriver.Chrome(options=my_options)
         return driver_setup
 
@@ -958,6 +958,7 @@ class AccordExtractor:
             temp_formated_1 = date
         counter = 0
         for i in temp_formated_1:
+            counter += 1
             if counter == 6 and i not in ['/', '-']:
                 temp_formated_2 = temp_formated_1[0:3] + '0' + temp_formated_1[3:]
                 change_2 = True
