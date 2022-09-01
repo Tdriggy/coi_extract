082fcaed631ac89de82350a6d3ee6fa73adf91f7
Tyler Driggers <109292821+Wtdrigg@users.noreply.github.com>
2022-09-01 07:50:02 -0500

    further small changes

--- accord_extractor.py
+++ accord_extractor.py
@@ -19,17 +19,22 @@ class AccordExtractor:
         self.pdf.load(0)
         self.driver = self.chromedriver_setup()
 
-    coordinate_dict = {'insurer_a':        (4.91, 8.56, 6.69, 8.56),
-                       'insurer_b':        (4.91, 8.39, 6.69, 8.39),
-                       'insurer_c':        (4.91, 8.22, 6.69, 8.22),
-                       'insurer_d':        (4.91, 8.06, 6.69, 8.06),
-                       'insurer_e':        (4.91, 7.90, 6.69, 7.90),
-                       'insurer_f':        (4.91, 7.73, 6.69, 7.73),
+    coordinate_dict = {'verify_accord':    (2.73, 0.35, 4.73, 0.46),
+                       'insurer_a':        (4.83, 8.55, 7.26, 8.58),
+                       'insurer_b':        (4.83, 8.34, 7.26, 8.38),
+                       'insurer_c':        (4.83, 8.20, 7.26, 8.22),
+                       'insurer_d':        (4.83, 8.06, 7.26, 8.08),
+                       'insurer_e':        (4.83, 7.90, 7.26, 7.92),
+                       'insurer_f':        (4.83, 7.73, 7.26, 7.75),
+                       'agent_name':       (4.82, 9.23, 6.46, 9.27),
+                       'agency_name':      (0.30, 9.08, 2.85, 9.10),
+                       'agent_phone':      (4.90, 9.04, 6.14, 9.07),
+                       'agent_email':      (4.81, 8.88, 5.77, 8.90),
                        'gl_pol_number':    (2.95, 5.75, 4.58, 6.73),
                        'gl_eff_date':      (4.65, 5.82, 5.16, 6.82),
                        'gl_exp_date':      (5.25, 5.84, 5.80, 6.80),
                        'gl_each_occur':    (7.37, 6.73, 8.17, 6.76),
-                       'gl_op_agg':        (7.31, 5.87, 7.80, 5.87),
+                       'gl_op_agg':        (7.30, 5.87, 7.80, 5.95),
                        'gl_insurer':       (0.27, 5.91, 0.45, 6.81),
                        'al_pol_number':    (2.95, 4.90, 4.58, 5.57),
                        'al_eff_date':      (4.86, 5.02, 4.86, 5.56),
@@ -40,8 +45,9 @@ class AccordExtractor:
                        'umb_eff_date':     (4.61, 4.40, 5.25, 4.73),
                        'umb_exp_date':     (5.25, 4.40, 5.90, 4.73),
                        'umb_each_occur':   (7.51, 4.71, 8.15, 4.77),
+                       'umb_aggregate':    (7.51, 4.55, 8.15, 4.60),
                        'umb_insurer':      (0.27, 4.41, 0.44, 4.81),
-                       'wc_pol_number:':   (3.00, 3.64, 4.52, 4.24),
+                       'wc_pol_number:':   (3.00, 3.72, 4.52, 4.24),
                        'wc_eff_date':      (4.60, 3.80, 5.25, 4.24),
                        'wc_exp_date':      (5.26, 3.80, 5.90, 4.24),
                        'wc_each_acci':     (7.31, 4.00, 8.25, 4.07),
@@ -52,8 +58,8 @@ class AccordExtractor:
 
     def chromedriver_setup(self):
         my_options = webdriver.ChromeOptions()
-        my_options.add_argument('user-data-dir=C:/Users/Tyler/ProgramingProjects/coi_extract/User Data')
-        driver_setup = webdriver.Chrome(options=my_options)
+        my_options.add_argument('user-data-dir=C:/Users/driggerst/Python/coi_extract/User Data')
+        driver_setup = webdriver.Chrome(options=my_options, executable_path='C:/Users/driggerst/Python/coi_extract/chromedriver.exe')
         return driver_setup
 
     # noinspection PyBroadException
@@ -66,7 +72,7 @@ class AccordExtractor:
         submit.click()
         try:
             print('Detecting Operation...')
-            WebDriverWait(self.driver, 5).until(ec.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[2]/div/'
+            WebDriverWait(self.driver, 7).until(ec.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[2]/div/'
                                                                                      'div[2]')))
         except Exception:
             try:
@@ -249,30 +255,56 @@ class AccordExtractor:
         return extracted_text
 
     def extract_text(self, xpath_map_arg):
-        insurer_dict = self.get_insurers()
-        self.get_gl_policy_number(self.coordinate_dict['gl_pol_number'], xpath_map_arg)
-        self.get_gl_eff_date(self.coordinate_dict['gl_eff_date'], xpath_map_arg)
-        self.get_gl_exp_date(self.coordinate_dict['gl_exp_date'], xpath_map_arg)
-        self.get_gl_each_occurrence(self.coordinate_dict['gl_each_occur'], xpath_map_arg)
-        self.get_gl_op_agg(self.coordinate_dict['gl_op_agg'], xpath_map_arg)
-        self.get_gl_insurer(self.coordinate_dict['gl_insurer'], insurer_dict, xpath_map_arg)
-        self.get_al_policy_number(self.coordinate_dict['al_pol_number'], xpath_map_arg)
-        self.get_al_eff_date(self.coordinate_dict['al_eff_date'], xpath_map_arg)
-        self.get_al_exp_date(self.coordinate_dict['al_exp_date'], xpath_map_arg)
-        self.get_al_comb_limit(self.coordinate_dict['al_comb_limit'], xpath_map_arg)
-        self.get_al_insurer(self.coordinate_dict['al_insurer'], insurer_dict, xpath_map_arg)
-        self.get_umb_pol_number(self.coordinate_dict['umb_pol_number'], xpath_map_arg)
-        self.get_umb_eff_date(self.coordinate_dict['umb_eff_date'], xpath_map_arg)
-        self.get_umb_exp_date(self.coordinate_dict['umb_exp_date'], xpath_map_arg)
-        self.get_umb_each_occur(self.coordinate_dict['umb_each_occur'], xpath_map_arg)
-        self.get_umb_insurer(self.coordinate_dict['umb_insurer'], insurer_dict, xpath_map_arg)
-        self.get_wc_pol_number(self.coordinate_dict['wc_pol_number:'], xpath_map_arg)
-        self.get_wc_eff_date(self.coordinate_dict['wc_eff_date'], xpath_map_arg)
-        self.get_wc_exp_date(self.coordinate_dict['wc_exp_date'], xpath_map_arg)
-        self.get_wc_each_acci(self.coordinate_dict['wc_each_acci'], xpath_map_arg)
-        self.get_wc_each_emp(self.coordinate_dict['wc_each_emp'], xpath_map_arg)
-        self.get_wc_pol_limit(self.coordinate_dict['wc_pol_limit'], xpath_map_arg)
-        self.get_wc_insurer(self.coordinate_dict['wc_insurer'], insurer_dict, xpath_map_arg)
+        accord_format = self.verify_accord(self.coordinate_dict['verify_accord'])
+        print()
+        if accord_format == 'Standard':
+            insurer_dict = self.get_insurers()
+            self.get_agent_name(self.coordinate_dict['agent_name'], xpath_map_arg)
+            self.get_agency_name(self.coordinate_dict['agency_name'], xpath_map_arg)
+            self.get_agent_phone(self.coordinate_dict['agent_phone'], xpath_map_arg)
+            self.get_agent_email(self.coordinate_dict['agent_email'], xpath_map_arg)
+            print()
+            self.get_gl_policy_number(self.coordinate_dict['gl_pol_number'], xpath_map_arg)
+            self.get_gl_eff_date(self.coordinate_dict['gl_eff_date'], xpath_map_arg)
+            self.get_gl_exp_date(self.coordinate_dict['gl_exp_date'], xpath_map_arg)
+            self.get_gl_each_occurrence(self.coordinate_dict['gl_each_occur'], xpath_map_arg)
+            self.get_gl_op_agg(self.coordinate_dict['gl_op_agg'], xpath_map_arg)
+            self.get_gl_insurer(self.coordinate_dict['gl_insurer'], insurer_dict, xpath_map_arg)
+            print()
+            self.get_al_policy_number(self.coordinate_dict['al_pol_number'], xpath_map_arg)
+            self.get_al_eff_date(self.coordinate_dict['al_eff_date'], xpath_map_arg)
+            self.get_al_exp_date(self.coordinate_dict['al_exp_date'], xpath_map_arg)
+            self.get_al_comb_limit(self.coordinate_dict['al_comb_limit'], xpath_map_arg)
+            self.get_al_insurer(self.coordinate_dict['al_insurer'], insurer_dict, xpath_map_arg)
+            print()
+            self.get_umb_pol_number(self.coordinate_dict['umb_pol_number'], xpath_map_arg)
+            self.get_umb_eff_date(self.coordinate_dict['umb_eff_date'], xpath_map_arg)
+            self.get_umb_exp_date(self.coordinate_dict['umb_exp_date'], xpath_map_arg)
+            self.get_umb_each_occur(self.coordinate_dict['umb_each_occur'], xpath_map_arg)
+            self.get_umb_aggregate(self.coordinate_dict['umb_aggregate'], xpath_map_arg)
+            self.get_umb_insurer(self.coordinate_dict['umb_insurer'], insurer_dict, xpath_map_arg)
+            print()
+            self.get_wc_pol_number(self.coordinate_dict['wc_pol_number:'], xpath_map_arg)
+            self.get_wc_eff_date(self.coordinate_dict['wc_eff_date'], xpath_map_arg)
+            self.get_wc_exp_date(self.coordinate_dict['wc_exp_date'], xpath_map_arg)
+            self.get_wc_each_acci(self.coordinate_dict['wc_each_acci'], xpath_map_arg)
+            self.get_wc_each_emp(self.coordinate_dict['wc_each_emp'], xpath_map_arg)
+            self.get_wc_pol_limit(self.coordinate_dict['wc_pol_limit'], xpath_map_arg)
+            self.get_wc_insurer(self.coordinate_dict['wc_insurer'], insurer_dict, xpath_map_arg)
+        elif accord_format == 'Alternate':
+            print('Dectected nonstandard ACCORD format, unable to process')
+
+    def verify_accord(self, coordinates):
+        verify = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+        print('verify is: ' + verify)
+        if verify == 'The ACORD name and logo are registered marks of ACORD':
+            verify_results = 'Standard'
+        elif verify == '':
+            verify_results = 'Alternate'
+        else:
+            verify_results = 'Unknown'
+        print('ACCORD Format: ' + verify_results)
+        return verify_results
 
     def get_insurers(self):
         insurer_a = self.pdf_extract(self.coordinate_dict['insurer_a'][0], self.coordinate_dict['insurer_a'][1],
@@ -297,11 +329,45 @@ class AccordExtractor:
         insurer_dict = {'A': insurer_a, 'B': insurer_b, 'C': insurer_c, 'D': insurer_d, 'E': insurer_e, 'F': insurer_f}
         return insurer_dict
 
-    def get_gl_policy_number(self, coordinates, xpath_map_arg):
+    def get_agent_name(self, coordinates, xpath_map_arg):
         click_button_element_test = self.driver.find_elements(By.XPATH, xpath_map_arg['click_box'])
         if click_button_element_test:
             click_button_element = self.driver.find_element(By.XPATH, xpath_map_arg['click_box'])
             click_button_element.click()
+        agent_name = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+        print('Agent Name: ' + agent_name)
+        agent_name_element = self.driver.find_element(By.XPATH, xpath_map_arg['agent_name'])
+        agent_name_element.clear()
+        agent_name_element.send_keys(agent_name)
+
+    def get_agency_name(self, coordinates, xpath_map_arg):
+        agency_name_temp = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+        agency_name = ''
+        for letter in agency_name_temp:
+            if letter not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',']:
+                agency_name += letter
+            else:
+                break
+        print('Agency Name: ' + agency_name)
+        agency_name_element = self.driver.find_element(By.XPATH, xpath_map_arg['agency_name'])
+        agency_name_element.clear()
+        agency_name_element.send_keys(agency_name)
+
+    def get_agent_phone(self, coordinates, xpath_map_arg):
+        agent_phone = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+        print('Agent Phone: ' + agent_phone)
+        agent_phone_element = self.driver.find_element(By.XPATH, xpath_map_arg['agent_phone'])
+        agent_phone_element.clear()
+        agent_phone_element.send_keys(agent_phone)
+
+    def get_agent_email(self, coordinates, xpath_map_arg):
+        agent_email = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+        print('Agent Email: ' + agent_email)
+        agent_email_element = self.driver.find_element(By.XPATH, xpath_map_arg['agent_email'])
+        agent_email_element.clear()
+        agent_email_element.send_keys(agent_email)
+
+    def get_gl_policy_number(self, coordinates, xpath_map_arg):
         gl_number = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
         if gl_number[0:4] in ['X X ', 'x x ']:
             gl_number = gl_number[4:]
@@ -430,7 +496,7 @@ class AccordExtractor:
                 break
         if contained_dates != []:
             if len(contained_dates) > 1:
-                gl_exp_date = contained_dates[0]
+                gl_exp_date = contained_dates[1]
             else:
                 gl_exp_date = contained_dates[0]
         else:
@@ -457,7 +523,7 @@ class AccordExtractor:
     def get_gl_insurer(self, coordinates, insurer_dict, xpath_map_arg):
         gl_insurer = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
         if gl_insurer in ['A', 'B', 'C', 'D', 'E', 'F']:
-            print('GL Insurer: ' + insurer_dict[gl_insurer] +'\n')
+            print('GL Insurer: ' + insurer_dict[gl_insurer])
             gl_insurer_element = self.driver.find_element(By.XPATH, xpath_map_arg['gl_insurer'])
             gl_insurer_element.clear()
             gl_insurer_element.send_keys(insurer_dict[gl_insurer])
@@ -593,7 +659,7 @@ class AccordExtractor:
                 break
         if contained_dates != []:
             if len(contained_dates) > 1:
-                al_exp_date = contained_dates[0]
+                al_exp_date = contained_dates[1]
             else:
                 al_exp_date = contained_dates[0]
         else:
@@ -613,7 +679,7 @@ class AccordExtractor:
     def get_al_insurer(self, coordinates, insurer_dict, xpath_map_arg):
         al_insurer = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
         if al_insurer in ['A', 'B', 'C', 'D', 'E', 'F']:
-            print('AL Insurer: ' + insurer_dict[al_insurer] +'\n')
+            print('AL Insurer: ' + insurer_dict[al_insurer])
             al_insurer_element = self.driver.find_element(By.XPATH, xpath_map_arg['al_insurer'])
             al_insurer_element.clear()
             al_insurer_element.send_keys(insurer_dict[al_insurer])
@@ -745,13 +811,13 @@ class AccordExtractor:
                         list_item += 1
             except IndexError:
                 break
-            if contained_dates != []:
-                if len(contained_dates) > 1:
-                    umb_exp_date = contained_dates[0]
-                else:
-                    umb_exp_date = contained_dates[0]
+        if contained_dates != []:
+            if len(contained_dates) > 1:
+                umb_exp_date = contained_dates[1]
             else:
-                umb_exp_date = ''
+                umb_exp_date = contained_dates[0]
+        else:
+            umb_exp_date = ''
         print('UMB Exp Date: ' + umb_exp_date)
         umb_exp_date_element = self.driver.find_element(By.XPATH, xpath_map_arg['umb_exp_date'])
         umb_exp_date_element.clear()
@@ -763,11 +829,18 @@ class AccordExtractor:
         umb_each_occur_element = self.driver.find_element(By.XPATH, xpath_map_arg['umb_each_occur'])
         umb_each_occur_element.clear()
         umb_each_occur_element.send_keys(umb_each_occur)
+    
+    def get_umb_aggregate(self, coordinates, xpath_map_arg):
+        umb_aggregate = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+        print('UMB Aggregate: ' + umb_aggregate)
+        umb_aggregate_element = self.driver.find_element(By.XPATH, xpath_map_arg['umb_aggregate'])
+        umb_aggregate_element.clear()
+        umb_aggregate_element.send_keys(umb_aggregate)
 
     def get_umb_insurer(self, coordinates, insurer_dict, xpath_map_arg):
         umb_insurer = self.pdf_extract(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
         if umb_insurer in ['A', 'B', 'C', 'D', 'E', 'F']:
-            print('UMB Insurer: ' + insurer_dict[umb_insurer] +'\n')
+            print('UMB Insurer: ' + insurer_dict[umb_insurer])
             umb_insurer_element = self.driver.find_element(By.XPATH, xpath_map_arg['umb_insurer'])
             umb_insurer_element.clear()
             umb_insurer_element.send_keys(insurer_dict[umb_insurer]) 
@@ -903,7 +976,7 @@ class AccordExtractor:
                 break
         if contained_dates != []:
             if len(contained_dates) > 1:
-                wc_exp_date = contained_dates[0]
+                wc_exp_date = contained_dates[1]
             else:
                 wc_exp_date = contained_dates[0]
         else:
--- coi_extract.py
+++ coi_extract.py
@@ -1,17 +1,22 @@
 import glob
 import os
 import time
+import traceback
+import sys
 from accord_extractor import AccordExtractor
 
 if __name__ == '__main__':
-
-    print()
-    files_in_downloads = glob. glob('C:/Users/Tyler/Downloads/*.pdf')
-    most_recent_file = max(files_in_downloads, key=os. path. getctime)
-    extractor = AccordExtractor(most_recent_file)
-    extractor.access_riskonnect()
-    operation_type = extractor.determine_type()
-    xpath_map = extractor.create_map(operation_type)
-    extractor.extract_text(xpath_map)
-    print()
-    time.sleep(1000000)
+    try:
+        print()
+        files_in_downloads = glob. glob('C:/Users/driggerst/Downloads/*.pdf')
+        most_recent_file = max(files_in_downloads, key=os. path. getctime)
+        extractor = AccordExtractor(most_recent_file)
+        extractor.access_riskonnect()
+        operation_type = extractor.determine_type()
+        xpath_map = extractor.create_map(operation_type)
+        extractor.extract_text(xpath_map)
+        print()
+        time.sleep(1000000)
+    except:
+        print(traceback.format_exc())
+        x = input('\nException detected, press Enter to close.')
